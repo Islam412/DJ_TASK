600839d21e54a60d5863c19c47295a7e01eda2b8
Islam412 <islam.124.hamdy@gmail.com>
2023-09-09 22:05:47 +0300

    add list template , url qusetions_list in setting

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Qusetion , Answer
 
 # Create your views here.
+
+
+def qusetions_list(request):
+    data = Qusetion.objects.all()
+    return render(request,'forum/list.html',{'data':data})
--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from forum.views import qusetions_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('qusetions/', qusetions_list)
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
